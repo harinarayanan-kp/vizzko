09e8c8b5d530ca65d50e92226ef1de6802484028
Harinarayanan K P <harikpaug18@gmail.com>
2025-05-25 19:16:50 +0530

    Add signup page component with form and Google login option

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import '../styles/login.css'
-import Navbar from '../components/navbar'
 
 type Props = {}
 
 const Login = () => {
     return (
         <div className='login-main'>
-            <Navbar />
             <div className='login-container'>
-                <h1 className='login-title'>Login</h1>
+                <h1 className='login-title'>Signup</h1>
                 <form className='login-form'>
                     <input type="text" placeholder='Username' className='login-input' />
                     <input type="password" placeholder='Password' className='login-input' />
+                    <input type="password" placeholder='Confirm Password' className='login-input' />
                     <div style={{ textAlign: 'right', marginBottom: '1em' }}>
                         <a href="/forgot-password" style={{ textDecoration: 'none', fontSize: '0.9em' }}>Forgot password?</a>
                     </div>
